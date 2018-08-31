75ec83c1cde044754fabee61ea4f404b523a7b3d
Richard MacLean <rick_maclean@tsco.org>
2018-08-31 14:33:54 -0500

    correct a typo

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -78,7 +78,7 @@ var UIController = (function() {
                 '</div></div></div>';
             } else if (type === 'expense') {
                 element = DOMqueryStrings.expenseContainer;
-                html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%t</div>' + 
+                html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div>' + 
                 '<div class="right clearfix"><div class="item__value">%value%</div>' + 
                 '<div class="item__percentage">21%</div><div class="item__delete">' + 
                 '<button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div>' + 
